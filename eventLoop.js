f431800768ccc0878b47a79eda894be5a93e24e1
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2024-11-24 23:08:01 +0530

    added Armstrong program

---
+++ eventLoop.js
@@ -0,0 +1,25 @@
+// console.log("Start");
+// setImmediate(() => {
+//     console.log("Immediate");
+// });
+// setTimeout(() => {
+//     console.log("Timeout");
+// }, 0);
+// console.log("End");
+
+
+
+// console.log("Start");
+// setImmediate(() => {
+//     console.log("Immediate");
+// });
+// setTimeout(() => {
+//     console.log("Timeout");
+// }, 1000);
+// Promise.resolve().then(() => {
+//     console.log("Promise");
+// });
+// process.nextTick(() => {
+//     console.log("Next Tick");
+// });
+// console.log("End");
---
+++ programs/armstrong.js
@@ -0,0 +1,22 @@
+/*
+An Armstrong number (or narcissistic number) is a number that is equal to the sum of its own digits raised to the power of the number of digits.
+
+For example:
+153 = 1³ + 5³ + 3³
+9474 = 9⁴ + 4⁴ + 7⁴ + 4⁴
+*/
+
+function isArmstrongNumber(number) {
+    const digits = number.toString().split(''); // Split number into digits
+    const power = digits.length; // Get the number of digits
+    const sum = digits.reduce((acc, digit) => acc + Math.pow(Number(digit), power), 0);
+    
+    return sum === number; // Check if the sum equals the original number
+}
+
+const number = 153;
+if (isArmstrongNumber(number)) {
+    console.log(`${number} is an Armstrong number.`);
+} else {
+    console.log(`${number} is not an Armstrong number.`);
+}
