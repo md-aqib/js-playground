c0126e96379bb6ba427a25cb6aea6a4d4df1dcfa
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2024-12-02 22:53:01 +0530

    added Promise APIs

--- nodejs/callback.js
+++ nodejs/callback.js
@@ -1,13 +1,24 @@
-function greet(name, callback) {
-    console.log(`Hello, ${name}!`);
-    callback();
-}
+// function greet(name, callback) {
+//     console.log(`Hello, ${name}!`);
+//     callback();
+// }
 
-function sayBye() {
-    console.log("Goodbye!");
-}
+// function sayBye() {
+//     console.log("Goodbye!");
+// }
 
-greet("Alice", sayBye);
+// greet("Alice", sayBye);
 // Output:
 // Hello, Alice!
 // Goodbye!
+
+
+function abc(sum) {
+    return sum(2, 3)
+}
+
+function sum(a,b) {
+    return a + b;
+}
+
+console.log(abc(sum))
--- nodejs/promises.js
+++ nodejs/promises.js
@@ -1,12 +1,36 @@
-const promise = new Promise((resolve, reject) => {
-    let success = false;
-    if (success) {
-        resolve("Operation successful!");
-    } else {
-        reject("Operation failed.");
-    }
-});
+// const promise = new Promise((resolve, reject) => {
+//     let success = false;
+//     if (success) {
+//         resolve("Operation successful!");
+//     } else {
+//         reject("Operation failed.");
+//     }
+// });
 
-promise
-    .then((message) => console.log(message))  // Runs on success
-    .catch((error) => console.log(error));    // Runs on failure
+// promise
+//     .then((message) => console.log(message))  // Runs on success
+//     .catch((error) => console.log(error));    // Runs on failure
+
+Promise.all([Promise.resolve(1), Promise.resolve(2)])
+    .then(console.log) // [1, 2]
+    .catch(console.error);
+
+
+Promise.allSettled([Promise.resolve(1), Promise.reject('Error')])
+  .then(console.log);
+// [{ status: 'fulfilled', value: 1 }, { status: 'rejected', reason: 'Error' }]
+
+
+Promise.race([
+    new Promise((res) => setTimeout(() => res('First'), 100)),
+    new Promise((_, rej) => setTimeout(() => rej('Error'), 50))
+]).then(console.log).catch(console.error); // "Error"
+
+
+Promise.any([
+    Promise.reject('Error 1'),
+    Promise.resolve('Success'),
+    Promise.reject('Error 2')
+]).then(console.log).catch(console.error); // "Success"
+  
+  
