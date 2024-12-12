29fa99b4a00ea95e43f0205e75f1a21762bde1de
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2024-12-12 22:38:55 +0530

    updated hashtable

--- dataStructures/hashMap.js
+++ dataStructures/hashMap.js
@@ -1,40 +1,37 @@
 // HASH TABLE
-  
+
 class HashTable {
   constructor(size) {
-    this.buckets = Array(size) 
-
-    // populate each bucket with a Map()
+    this.buckets = Array(size);
     for (let i = 0; i < this.buckets.length; i++) {
-      this.buckets[i] = new Map()
+      this.buckets[i] = new Map();
     }
   }
 
-  // Hash function
   hash(key) {
-      let hashValue = 0;
-      for (let char of key) {
-          hashValue += char.charCodeAt(0);
-      }
-      return hashValue % this.buckets.length;
+    let hashValue = 0;
+    for(let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i)
+    }
+    return hashValue % this.buckets.length;
   }
 
   insert(key, value) {
-    let idx = this.hash(key) 
+    let idx = this.hash(key);
     console.log({idx})
     this.buckets[idx].set(key, value)
   }
 
-  remove(key) {
-    let idx = this.hash(key)
-    let deleted = this.buckets[idx].get(key)
-    this.buckets[idx].delete(key)
-    return deleted 
+  search(key) {
+    let idx = this.hash(key);
+    return this.buckets[idx].get(key); 
   }
 
-  search(key) {
-    let idx = this.hash(key)
-    return this.buckets[idx].get(key)
+  remove(key) {
+    let idx = this.hash(key);
+    let deleted = this.buckets[idx].get(key);
+    this.buckets[idx].delete(key);
+    return deleted; 
   }
 }
 
@@ -47,11 +44,11 @@ hashTable.insert('lita', 'jupiter')
 hashTable.insert('mina', 'venus')
 hashTable.insert('darien', 'tuxedo mask')
 
-hashTable.search('rei')
-//   hashTable.search('lita')
-//   hashTable.search('serena')
+// hashTable.search('rei')
+// hashTable.search('lita')
+// hashTable.search('serena')
 
-//   hashTable.remove('darien')
-//   hashTable.remove('mina')
+hashTable.remove('darien')
+hashTable.remove('mina')
 
-console.log(hashTable.search('rei'))
+console.log(hashTable)
