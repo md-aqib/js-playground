ecf000aba5c3558114e4765d842b3e74edf58122
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2025-01-11 16:26:55 +0530

    update

---
+++ programs/traverse.js
@@ -0,0 +1,24 @@
+const nestedObject = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3
+      }
+    },
+    f: 4
+};
+  
+  function traverse(obj) {
+    for (const key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        // Recursive call for nested objects
+        traverse(obj[key]);
+      } else {
+        console.log(`${key}: ${obj[key]}`);
+      }
+    }
+}
+  
+traverse(nestedObject);
+  
