99a4c0d0d20c2b36e4bc363c807f7b3b51be51a5
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2025-01-30 23:14:03 +0530

    update

--- programs/findAllOccurrance.js
+++ programs/findAllOccurrance.js
@@ -1,14 +1,30 @@
+// function findAllOccurrences(inputString, searchString) {
+//     const result = [];
+//     let index = inputString.indexOf(searchString);
+  
+//     // Loop to find all occurrences
+//     while (index !== -1) {
+//       result.push(index);
+//       index = inputString.indexOf(searchString, index + 1); // Search from the next position
+//     }
+  
+//     return result;
+// }
+
 function findAllOccurrences(inputString, searchString) {
-    const result = [];
-    let index = inputString.indexOf(searchString);
-  
-    // Loop to find all occurrences
-    while (index !== -1) {
-      result.push(index);
-      index = inputString.indexOf(searchString, index + 1); // Search from the next position
+  const result = [];
+  let arrOfStr = inputString.split(" ");
+  let currIndex = 0;
+  for(let ele of arrOfStr) {
+    if(ele === searchString) {
+      result.push(currIndex);
+      currIndex += ele.length + 1;
+    } else {
+      currIndex += ele.length + 1;
     }
-  
-    return result;
+  }
+
+  return result;
 }
 
 const inputString = "hello hello world hello";
