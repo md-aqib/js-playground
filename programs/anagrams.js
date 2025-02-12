5b3e69690ef529f39f6d7506a9ca9005f1454567
Mohd Aqib <aqib@Mohds-MacBook-Air.local>
2025-02-12 11:02:31 +0530

    update

---
+++ programs/anagrams.js
@@ -0,0 +1,16 @@
+function groupAnagrams(words) {
+    let anagramMap = new Map();
+
+    for (let word of words) {
+        let sortedWord = word.split('').sort().join(''); // Sort characters
+        if (!anagramMap.has(sortedWord)) {
+            anagramMap.set(sortedWord, []);
+        }
+        anagramMap.get(sortedWord).push(word);
+    }
+
+    return Array.from(anagramMap.values());
+}
+
+const words = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(words));
